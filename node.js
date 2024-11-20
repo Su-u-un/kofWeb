cd0d6aab777c17c6ed2c2613f0a757dbac430317
Johnny <tl071515@gmail.com>
2024-11-20 17:59:54 +0800

    1

---
+++ node.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const path = require('path');
+const https = require('https');
+const fs = require('fs')
+app.use(cors());
+// 创建数据库连接
+  app.use(express.static(path.join(__dirname, './frontend')));
+  app.get('/', (req, res) => {
+    
+    res.redirect('/index.html');
+  });
+
+const options = {
+  key: fs.readFileSync('./ssl/kof.key'),
+  cert: fs.readFileSync('./ssl/kof.crt'),
+  ca: fs.readFileSync('./ssl/kof.ca-bundle')
+};
+
+https.createServer(options, app).listen(443,()=>{console.log('Server is running on https://localhost:443');})
+ 
